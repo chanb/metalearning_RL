1f053ae4051837e951b623948770ab574256af44
Chantal S <chantal.shaib@gmail.com>
2018-11-20 15:55:10 -0500

    plot works now yay

--- read_result.py
+++ read_result.py
@@ -15,27 +15,28 @@ args = parser.parse_args()
 
 with open(args.file, 'rb') as f:  # Python 3: open(..., 'rb')
     all_rewards, all_actions, all_states, num_actions, num_states = pickle.load(f)
-
     # convert to a numpy matrix
     all_rewards_matrix = np.array([np.array(xi) for xi in all_rewards])
     # each row now contains values for each iteration
     all_rewards_matrix = all_rewards_matrix.T
+    all_rewards_matrix = np.cumsum(all_rewards_matrix, axis=0)
+    #one_task = all_rewards_matrix[1][:]
     # computes std dev of each row
-    #reward_err = np.std(all_rewards_matrix, axis=1)
+   # reward_err = np.std(all_rewards_matrix, axis=1)
     avg_reward = np.average(all_rewards_matrix, axis=1)
-    # # normalizing
-    # avg_reward = (avg_reward - (avg_reward.min()))/(avg_reward.max() - avg_reward.min())
-    # reward_err = (reward_err - (reward_err.min()))/(reward_err.max() - reward_err.min())
 
-    #print(avg_reward / 100)
+    # normalizing
+    for i in range(len(avg_reward)):
+        avg_reward[i] = avg_reward[i]/(i+1)
+        #reward_err[i] = reward_err[i]/(i+1)
 
     # plotting
     plt.plot(range(len(avg_reward)), avg_reward)
-   #plt.plot(x=len(avg_reward), y=reward_err)
+    #plt.scatter(range(len(one_task)), one_task)
     plt.xlabel('Number of Iterations')
     plt.ylabel('Total Reward')
     plt.title('Model Performance')
-   #plt.fill_between(x=range(len(avg_reward)), y1=reward_err, color = 'gray')
+    #plt.fill_between(x=range(len(avg_reward)), y1=reward_err, color = 'gray')
     plt.show()
 
     # print(all_rewards)
