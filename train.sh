d863704fed02bdad87bcc876e9908e6ddcd17092
Angeline <angelineyasodhara@gmail.com>
2018-11-19 20:34:19 -0500

    Update train.sh 2

--- train.sh
+++ train.sh
@@ -16,7 +16,7 @@ done
 trajs=( 10 25 50 75 100 )
 
 for traj in ${trajs[@]}; do
-    python rl2.py --num_tasks $num_tasks --max_num_traj 10 --max_num_traj_eval 0 --learning_rate 0.001 --algo ppo --ppo_epochs 2 --clip_param 0.2 --tau 0.3 --max_traj_len 10 --task mdp --eval 0
-    python snail.py --num_tasks $num_tasks --max_num_traj 10 --max_num_traj_eval 0 --learning_rate 0.001 --algo ppo --ppo_epochs 2 --clip_param 0.2 --tau 0.3 --max_traj_len 10 --task mdp --eval 0
+    python rl2.py --num_tasks $num_tasks --max_num_traj 10 --max_num_traj_eval 0 --learning_rate 0.001 --algo reinforce --max_traj_len 10 --task mdp --eval 0
+    python snail.py --num_tasks $num_tasks --max_num_traj 10 --max_num_traj_eval 0 --learning_rate 0.001 --algo reinforce --max_traj_len 10 --task mdp --eval 0
     python maml.py --env-name TabularMDP-v0 --fast-batch-size $traj
 done
