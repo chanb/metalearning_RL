21bd207a77ddc28771372bd1ec03beab8dd4f84a
chanb <bryanchan13@gmail.com>
2018-11-20 13:10:10 -0500

    Separated evaluation and training

--- snail.py
+++ snail.py
@@ -35,19 +35,13 @@ parser.add_argument('--task', type=str, default='bandit', help='the task to lear
 parser.add_argument('--max_num_traj_eval', type=int, default=1000,
                     help='maximum number of trajectories during evaluation (default: 1000)')
 parser.add_argument('--clip_param', type=float, default=0.2, help='clipping parameter for PPO (default: 0.2)')
-parser.add_argument('--eval', type=int, default=1, help='do evaluation only (default: 1)')
 parser.add_argument('--non_linearity', help='non linearity function following last output layer')
-
-parser.add_argument('--eval_model', help='the model to evaluate')
-parser.add_argument('--eval_tasks', help='the tasks to evaluate on')
 
 args = parser.parse_args()
 
 eps = np.finfo(np.float32).eps.item()
 out_folder = './saves/snail'
 out_model = '{}/{}_{}_{}_{}.pt'.format(out_folder, args.algo, args.task, args.num_actions, args.max_num_traj)
-result_folder = './logs/snail'
-out_result = '{}/{}_{}_{}_{}.pkl'.format(result_folder, args.algo, args.task, args.num_actions, args.max_num_traj)
 
 
 def meta_train():
@@ -93,76 +87,6 @@ def meta_train():
         torch.save(model, out_model)
 
 
-def evaluate_model(eval_model, eval_tasks=None):
-    to_use = out_model
-    if (eval_model):
-        to_use = eval_model
-
-    model = torch.load(to_use)
-    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate)
-
-    task = ''
-    if args.task == 'bandit':
-        task = "Bandit-K{}-v0".format(args.num_actions)
-        num_actions = args.num_actions
-        num_states = 1
-    elif args.task == 'mdp':
-        task = "TabularMDP-v0"
-        num_actions = 5
-        num_states = 10
-    else:
-        print('Invalid Task')
-        return
-
-    if model.is_recurrent:
-        model.reset_hidden_state()
-
-    env = gym.make(task)
-    tasks = env.unwrapped.sample_tasks(1)
-    if (eval_tasks):
-        with open(eval_tasks, 'rb') as f:
-            tasks = pickle.load(f)[0]
-    
-    if args.algo == 'reinforce':
-        all_rewards, all_states, all_actions, _ = reinforce(model, optimizer, task, num_actions, 1,
-                                                            args.max_num_traj_eval, args.max_traj_len,
-                                                            args.gamma, evaluate_tasks=tasks)
-    elif args.algo == 'ppo':
-        all_rewards, all_states, all_actions, _ = ppo(model, optimizer, task, num_actions, 1, args.max_num_traj_eval,
-                                                      args.max_traj_len,
-                                                      args.ppo_epochs, args.mini_batch_size, args.gamma, args.tau,
-                                                      args.clip_param, evaluate=True)
-    else:
-        print('Invalid learning algorithm')
-
-    if not os.path.exists(result_folder):
-        os.makedirs(result_folder)
-
-    with open(out_result, 'wb') as f:
-        pickle.dump([all_rewards, all_actions, all_states, num_actions, num_states], f)
-
-    idx = 0
-    for traj in all_states[0]:
-        idx += 1
-        curr_traj = traj
-        print('traj {} (length: {}) reward {} actions_made {}: '.format(idx, len(traj), all_rewards[0][idx - 1],
-                                                                        all_actions[0][0]))
-        if (args.algo == 'ppo'):
-            curr_traj = traj.squeeze(1)
-            for experience in curr_traj:
-                print('curr_state: {} prev_action: {} prev_reward: {} is_done: {}'.format(experience[:num_states],
-                                                                                          experience[
-                                                                                          num_states:num_states + num_actions],
-                                                                                          experience[
-                                                                                              num_states + num_actions],
-                                                                                          experience[-1]))
-    print(all_actions)
-    print(all_rewards)
-
-
 if __name__ == '__main__':
-    if (not args.eval):
-        print("TRAINING MODEL ========================================================================")
-        meta_train()
-    print("TESTING MODEL ========================================================================")
-    evaluate_model(args.eval_model, args.eval_tasks)
+    print("TRAINING MODEL ========================================================================")
+    meta_train()
