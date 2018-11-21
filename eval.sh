5a8009dc7fe3cef70173e55a838787612686f503
Angeline <angelineyasodhara@gmail.com>
2018-11-20 19:30:39 -0500

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -9,14 +9,30 @@ trajs=( 10 100 500 1000 )
 
 for task in ${tasks[@]}; do
     if [ $task == "bandit" ]; then
-        for arm in ${arms[@]}; do
-            python generate_experiments.py --num_tasks $num_tasks --num_actions $arm --task $task
-        done
-    else
-        python generate_experiments.py --num_tasks $num_tasks --num_actions 5 --task $task
-    fi
+    actions=$arms
+     else
+        actions=( 5 )
+     fi
+    for action in ${actions[@]}; do
+        python generate_experiments.py --num_tasks $num_tasks --num_actions $action --task $task
+    done
 done
 
-for algo in ${algos[@]}; do
-    python evaluate_model.py --num_tasks $num_tasks --num_actions $arm --task $task --eval_model --eval_tasks ./experiments/"$task"_"$num_actions"_"$num_tasks"
+for task in ${tasks[@]}; do
+    if [ $task == "bandit" ]; then
+        actions=$arms
+    else
+       actions=( 5 )
+    fi
+    for traj in ${trajs[@]}; do
+        for action in ${actions[@]}; do
+            for algo in ${algos[@]}; do
+                python evaluate_model.py --num_tasks $num_tasks --num_actions $action --task $task \
+                    --eval_model ./saves/$algo/$task_"$action"_"$traj".pt
+                    --eval_tasks ./experiments/"$task"_"$action"_"$num_tasks".pkl
+                mkdir $algo/logs_eval/$algo
+                mv ./logs_eval/"$task"_"$action"_"$num_tasks".pkl ./logs_eval/$algo/"$task"_"$action"_"$traj".pkl
+            done
+        done
+    done
 done
