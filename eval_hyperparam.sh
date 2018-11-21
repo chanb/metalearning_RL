c2201e725a554ce2466a86bc1d3e581be4d056a9
Angeline <angelineyasodhara@gmail.com>
2018-11-20 21:44:53 -0500

    Update eval_hyperparam.sh

--- eval_hyperparam.sh
+++ eval_hyperparam.sh
@@ -8,23 +8,24 @@ num_tasks=( 50 100 200 )
 tasks=( bandit mdp )
 
 for task in ${tasks[@]}; do
+    python generate_experiments.py --num_tasks 100 --num_actions $arm --task $task
     for num_task in ${num_tasks[@]}; do
         for lr in ${lrs[@]}; do
            model=reinforce_$task"_"$arm"_"$traj"_"adam"_"lr$lr"_"numtasks$num_task
-           #python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
-                    #--eval_model ./saves/rl2/$model.pt
-                    #--eval_tasks ./experiments/"$task"_"$action"_"$num_tasks".pkl
-                    #--out_file ./logs_eval/rl2/$model.pkl
+           python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
+                    --eval_model ./saves/rl2/$model.pt
+                    --eval_tasks ./experiments/"$task"_"$action"_100.pkl
+                    --out_file ./logs_eval/rl2/$model
 
            python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
                     --eval_model ./saves/snail/$model.pt
-                    --eval_tasks ./experiments/"$task"_"$action"_"$num_tasks".pkl
-                    --out_file ./logs_eval/snail/$model.pkl
+                    --eval_tasks ./experiments/"$task"_"$action"_100.pkl
+                    --out_file ./logs_eval/snail/$model
         done
-        #model=reinforce_$task"_"$arm"_"$traj"_"numtasks$num_task
-        #python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
-                    #--eval_model ./saves/maml/$model.pt
-                    #--eval_tasks ./experiments/"$task"_"$action"_"$num_tasks".pkl
-                    #--out_file ./logs_eval/maml/$model.pkl
+        model=reinforce_$task"_"$arm"_"$traj"_"numtasks$num_task
+        python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
+                    --eval_model ./saves/maml/$model.pt
+                    --eval_tasks ./experiments/"$task"_"$action"_100.pkl
+                    --out_file ./logs_eval/maml/$model
     done
 done
