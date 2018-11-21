e3d711f482bf725f2efcc27f9be230f3e3f3dd1c
Angeline <angelineyasodhara@gmail.com>
2018-11-20 19:48:18 -0500

    Changed the name of the saved policies because we're gonna try with adam

---
+++ search_hyperparam.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Find the best learning rate
+num_tasks=100
+arms=( 5 )
+trajs=( 10 )
+
+for traj in ${trajs[@]}; do
+    for arm in ${arms[@]}; do
+        echo $arm
+        echo $traj
+        python rl2.py --learning_rate 0.001 --algo reinforce --task bandit --num_actions $arm --max_traj_len 1 --max_num_traj $traj --num_tasks $num_tasks
+        python snail.py --learning_rate 0.001 --algo reinforce --task bandit --num_actions $arm --max_traj_len 1 --max_num_traj $traj --num_tasks $num_tasks
+        python maml.py -task bandit --num_actions $arm --fast-batch-size $traj --num-batches $num_tasks
+    done
+done
+
+trajs=( 10 25 50 75 100 )
+for traj in ${trajs[@]}; do
+    echo $traj
+    python rl2.py --num_tasks $num_tasks --max_num_traj 10 --learning_rate 0.001 --algo reinforce --max_traj_len 10 --task mdp
+    python snail.py --num_tasks $num_tasks --max_num_traj 10 --learning_rate 0.001 --algo reinforce --max_traj_len 10 --task mdp
+    python maml.py -task mdp --fast-batch-size $traj --num-batches $num_tasks
+done
