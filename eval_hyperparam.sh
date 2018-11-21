9afb4b5a3d9d1acbe7654f4eceb2f32c6a605f42
Angeline <angelineyasodhara@gmail.com>
2018-11-20 22:18:21 -0500

    Fix typo

--- eval_hyperparam.sh
+++ eval_hyperparam.sh
@@ -14,7 +14,7 @@ for task in ${tasks[@]}; do
            model=reinforce_$task"_"$arm"_"$traj"_"adam"_"lr$lr"_"numtasks$num_task
            python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
                     --eval_model ./saves/rl2/$model.pt \
-                    --eval_tasks ./experiments/"$task"_"$action"_100.pkl \
+                    --eval_tasks ./experiments/"$task"_"$arm"_100.pkl \
                     --outfile ./logs_eval/rl2/$model.pkl
 
            python read_result.py --task $task \
@@ -23,7 +23,7 @@ for task in ${tasks[@]}; do
 
            python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
                     --eval_model ./saves/snail/$model.pt \
-                    --eval_tasks ./experiments/"$task"_"$action"_100.pkl \
+                    --eval_tasks ./experiments/"$task"_"$arm"_100.pkl \
                     --outfile ./logs_eval/snail/$model.pkl
 
             python read_result.py --task $task \
@@ -33,7 +33,7 @@ for task in ${tasks[@]}; do
         model=reinforce_$task"_"$arm"_"$traj"_"numtasks$num_task
         python evaluate_model.py --num_tasks 100 --num_actions $arm --task $task \
                     --eval_model ./saves/maml/$model.pt \
-                    --eval_tasks ./experiments/"$task"_"$action"_100.pkl \
+                    --eval_tasks ./experiments/"$task"_"$arm"_100.pkl \
                     --outfile ./logs_eval/maml/$model.pkl
 
         python read_result.py --task $task \
