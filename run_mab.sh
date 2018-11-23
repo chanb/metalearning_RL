ad861cb5fc0270bc3efb4d41a09d083a2eb69952
chanb <bryanchan13@gmail.com>
2018-11-23 11:09:55 -0500

    Added variables for faster change

--- run_mab.sh
+++ run_mab.sh
@@ -1,8 +1,12 @@
 tasks=100
+lr=0.0002
+epochs=4
+batch=10
+minibatch=5
 
-python rl2.py --algo ppo --ppo_epochs 4 --task bandit --traj_len 1 --num_traj 100 --mini_batch_size 5 --learning_rate 0.0002 --batch_size 10 --num_tasks $tasks --clip_param 0.1
+python rl2.py --algo ppo --ppo_epochs $epochs --task bandit --traj_len 1 --num_traj 100 --mini_batch_size $minibatch --learning_rate $lr --batch_size $batch --num_tasks $tasks --clip_param 0.1
 
-python evaluate_model.py --num_tasks $tasks --num_actions 5 --task bandit --eval_model ./saves/rl2/ppo_bandit_5_100_SGD_lr0.0002_numtasks$tasks.pt --eval_tasks ./experiments/bandit_5_$tasks.pkl --outfile sgd_result.pkl --ppo_epochs 4 --task bandit --traj_len 1 --num_traj 100 --mini_batch_size 5 --learning_rate 0.0002 --batch_size 10 --clip_param 0.1
+python evaluate_model.py --num_tasks $tasks --num_actions 5 --task bandit --eval_model ./saves/rl2/ppo_bandit_5_100_SGD_lr${lr}_numtasks$tasks.pt --eval_tasks ./experiments/bandit_5_$tasks.pkl --outfile sgd_result.pkl --ppo_epochs $epochs --task bandit --traj_len 1 --num_traj 100 --mini_batch_size $minibatch --learning_rate $lr --batch_size $batch --clip_param 0.1
 
 
 python read_result.py --task bandit --file ./sgd_result.pkl --outfile ./sgd_plot_$tasks
