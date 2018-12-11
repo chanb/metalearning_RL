1465fb2e12ab07997ef9946119e665621f3dc222
chanb <bryanchan13@gmail.com>
2018-12-10 21:22:50 -0500

    Removed unused scripts and added a template experiment script

---
+++ train_on_comps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+arms=5
+tasks=bandit
+num_tasks=1000
+num_traj=10
+traj_len=1
+
+tau=0.3
+gamma=0.99
+ppo_epoch=5
+learning_rate=3e-4
+clip_param=0.1
+num_workers=1
+
+batchsize=10000
+minibatchsize=256
+metalearn_epoch=300
+
+models=( gru snail )
+
+# For each of the model, run MAB
+for model in ${models[@]}; do
+    policy=train_${model}_${arms}bandit_${num_traj}traj_${num_tasks}tasks
+    mkdir -p $policy
+
+    python rl2_train.py --model_type $model --out_file $policy.pt --batch_size $batchsize --num_tasks $num_tasks --mini_batch_size $minibatchsize --num_traj $num_traj --tau $tau --gamma $gamma --ppo_epochs $ppo_epoch --learning_rate $learning_rate --clip_param $clip_param --num_workers $num_workers --num_actions $arms --traj_len $traj_len --metalearn_epoch $metalearn_epoch
+
+    mv ./tmp/*_$policy.pt $policy
+    mv $policy.pt $policy
+done
