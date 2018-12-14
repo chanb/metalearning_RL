68dd7f8450a23d8baf98f6f24ca43465c8eb86ee
chanb <bryanchan13@gmail.com>
2018-12-14 14:28:48 -0500

    trying to implement my own snail

--- helper/model_init.py
+++ helper/model_init.py
@@ -22,3 +22,18 @@ class LinearEmbedding(nn.Module):
 
   def forward(self, x):
     return self.fcn(x)
+
+
+class CausalConv1D(nn.Module):
+  def __init__(self, input_size, dilation_rate, output_size, kernel_size=2, stride=1, groups=1, bias=True):
+    super(CausalConv1D, self).__init__()
+    self.dilation_rate = dilation_rate
+
+    padding = max(0, ((output_size - 1) * stride + kernel_size - input_size)//2)
+    print(padding)
+    self.conv1d = nn.Conv1d(input_size, output_size, kernel_size, stride, padding, dilation_rate, groups, bias)
+
+  def forward(self, x):
+    # Input shape: (batch_size N, input_size C, sequence_length T)
+    # Output shape: (batch_size N, output_size D, sequence length T)
+    return self.conv1d(x)[:, :, :-self.dilation_rate]
